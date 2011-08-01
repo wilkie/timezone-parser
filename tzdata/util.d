67a371f47cf935a8bf3db51c2292dbd0bd599f95
wilkie <wilkie05@gmail.com>
2011-08-01 14:50:12 -0400

    Adding D Utility functions.

---
+++ tzdata/util.d
@@ -0,0 +1,72 @@
+module tzdata.util;
+
+class Util {
+static:
+public:
+
+	uint isFirst(uint dayOfWeek, uint afterOrOnDay, long year, uint month) {
+		return false;
+	}
+
+	uint isLast(uint dayOfWeek, long year, uint month) {
+		return false;
+	}
+
+	bool isAfter (long year, uint month, uint day, 
+			uint hour, uint minute,  
+			long check_year, uint check_month, uint check_day, 
+			uint check_hour, uint check_minute) {
+
+		if (year >= check_year) {
+			if (month > check_month) {
+				return true;
+			}
+			else if (month == check_month) {
+				if (day > check_day) {
+					return true;
+				}
+				else if (day == check_day) {
+					if (hour > check_hour) {
+						return true;
+					}
+					else if (hour == check_hour) {
+						if (minute >= check_minute) {
+							return true;
+						}
+					}
+				}
+			}
+		}
+
+		return false;
+	}
+
+	bool isBefore (long year, uint month, uint day, 
+			uint hour, uint minute, 
+			long check_year, uint check_month, uint check_day, 
+			uint check_hour, uint check_minute) {
+
+		if (year <= check_year) {
+			if (month < check_month) {
+				return true;
+			}
+			else if (month == check_month) {
+				if (day < check_day) {
+					return true;
+				}
+				else if (day == check_day) {
+					if (hour < check_hour) {
+						return true;
+					}
+					else if (hour == check_hour) {
+						if (minute < check_minute) {
+							return true;
+						}
+					}
+				}
+			}
+		}
+
+		return false;
+	}
+}
