404dd589ea1f528cea73f6a2d1c8048ac251445a
wilkie <wilkie05@gmail.com>
2011-08-11 08:47:52 -0400

    Making preprocess more general.

--- preprocess.sh
+++ preprocess.sh
@@ -1,5 +1,15 @@
 mkdir -p tzdata
 cd tzdata
+wget 'ftp://elsie.nci.nih.gov/pub/tz*.tar.gz'
+gzip -dc tzdata*.tar.gz | tar -xf -
+rm -f factory
+rm -f leapseconds
+rm -f solar*
+rm -f *.sh
+rm -f *.tab
+rm -f systemv
+rm -f etcetera
+rm -f *.gz
 grep -E "^Zone\s+\S+\s+\S+\s+\S+\s+|^\s\s\s+[-0-9]" * > zones
 grep -E "^Rule\s+\S+\s+\S+\s+\S+\s+" * > rules
 cd ..
