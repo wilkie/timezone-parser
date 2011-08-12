15568ed0029ab413066aee9e42b0820a03987f5e
wilkie <wilkie05@gmail.com>
2011-08-12 11:32:15 -0400

    Add the isStrictlyBefore method.

--- tzcode/util.d
+++ tzcode/util.d
@@ -41,6 +41,38 @@ public:
 		return false;
 	}
 
+	bool isStrictlyBefore (long year, uint month, uint day, 
+			uint hour, uint minute, 
+			long check_year, uint check_month, uint check_day, 
+			uint check_hour, uint check_minute) {
+
+		if (year < check_year) {
+			return true;
+		}
+		else if (year == check_year) {
+			if (month < check_month) {
+				return true;
+			}
+			else if (month == check_month) {
+				if (day < check_day) {
+					return true;
+				}
+				else if (day == check_day) {
+					if (hour < check_hour) {
+						return true;
+					}
+					else if (hour == check_hour) {
+						if (minute < check_minute) {
+							return true;
+						}
+					}
+				}
+			}
+		}
+
+		return false;
+	}
+
 	bool isBefore (long year, uint month, uint day, 
 			uint hour, uint minute, 
 			long check_year, uint check_month, uint check_day, 
